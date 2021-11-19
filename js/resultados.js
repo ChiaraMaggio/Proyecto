724859bdbbb09dcbc86c8977ea56b04795416ffe
ChiaraMaggio <cmaggio@udesa.edu.ar>
2021-11-19 10:07:03 -0300

    resultados, fav, home

--- js/resultados.js
+++ js/resultados.js
@@ -9,7 +9,7 @@ window.addEventListener("load", function(){
         
     /*fetch para resultados de películas*/
 
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=a114d2c8656f1a238841af09c2a4f418&language=en-US&query=${busqueda}&page=1`)
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=a114d2c8656f1a238841af09c2a4f418&query=${busqueda}&page=1`)
     .then(function(response){
         return response.json();
     })
@@ -21,17 +21,17 @@ window.addEventListener("load", function(){
             document.querySelector("#leyenda2").innerHTML = `
             <h3>No se hallaron resultados para <span class="texto-verde">${busqueda}</span> en God Films</h3>
             `;
-        }
-        else{
+        }else{
             document.querySelector(".leyenda1").innerHTML += `
-            <h3>Buscaste <span class="texto-verde">${busqueda}</span> en God Films</h3>
-            `
+                <h3>Buscaste <span class="texto-verde">${busqueda}</span> en God Films</h3>
+            `;
+
             for (let i = 0; i < datos.results.length; i++) {
                 document.querySelector(".contenedor-géneros").innerHTML += `
                     <article class="artículo-género">
                         <div>
                             <p id="clasificación">Película</p>
-                            <a href="detail-movie.html?id=${datos.results[i].id}"><img src="https://image.tmdb.org/t/p/w200${datos.results[i].poster_path}" alt="Portada de ${datos.results[i].original_title}"></a>
+                            <a href="detail-movie.html?id=${datos.results[i].id}"><img id="portadas" src="https://image.tmdb.org/t/p/w200${datos.results[i].poster_path}" alt="Portada de ${datos.results[i].original_title}"></a>
                             <div class="nombre-estreno">
                                 <h4>${datos.results[i].original_title}</h4>
                                 <p>${datos.results[i].release_date}</p>
@@ -59,10 +59,10 @@ window.addEventListener("load", function(){
 
         if(datos.results.length == 0){
             document.querySelector("#leyenda2").innerHTML = `
-            <h3>No se hallaron resultados para <span class="texto-verde">${busqueda}</span> en God Films</h3>
+                <h3>No se hallaron resultados para <span class="texto-verde">${busqueda}</span> en God Films</h3>
             `;
-        }
-        else{
+        }else{
+
             for (let i = 0; i < datos.results.length; i++) {
                 document.querySelector(".contenedor-géneros").innerHTML += `
                     <article class="artículo-género">
